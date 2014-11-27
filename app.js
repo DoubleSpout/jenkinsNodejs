f48169f7462594f445a52f64e4c0a36376c089ae
spout <snoopyxdy@gmail.com>
2014-11-27 12:37:06 +0800

    tangren

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World o my god\n');
+  res.end('Hello World o my god tangren\n');
 }).listen(9001);
 console.log('Server running at http://127.0.0.1:9001/');
